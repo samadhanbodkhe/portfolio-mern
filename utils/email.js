4f5a407609afe8717dad59270e0fcff2a3f50d3f
unknown <bodkhesamadhan188@gmail.com>
2024-02-17 14:24:28 +0530

    sas

--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer")
 
 const sendEmail = ({
-    to,
+    to = process.env.FROM_EMAIL,
     message = "test email",
     subject = "test subject"
 }
@@ -17,7 +17,7 @@ const sendEmail = ({
         });
         console.log(to);
         miler.sendMail({
-            to: process.env.FROM_EMAIL,
+            to,
             subject,
             text: message,
             from: process.env.FROM_EMAIL
