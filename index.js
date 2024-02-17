82f68070f0f8c01880b1a19dcac7fb9e4010f42d
unknown <bodkhesamadhan188@gmail.com>
2024-02-17 13:19:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+require("dotenv").config({ path: "./.env" }
+)
+
+
+const app = express()
+app.use(express.static(path.join(__dirname, "dist")))
+app.use(express.json())
+app.use(cors())
+app.use("/api/admin", require("./routes/adminRoute"))
+
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    res.status(404).json({ message: "resource Not foudn" })
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message || "Something Went Wrring" })
+})
+
+app.listen(5000, console.log("server running"))
